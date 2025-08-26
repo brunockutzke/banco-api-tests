f33997a8af7a3fdde2097ea3e5ca79683f7f9a69
brunockutzke <brunokutzke@gmail.com>
2025-08-26 19:11:23 -0300

    Adicionando a biblioteca dotenv para variáveis globais.

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,13 @@
 const request = require('supertest')
 const { expect } = require('chai')
+require('dotenv').config()
 
 describe('Transferências', () => {
     describe('/POST /transferencias', () => {
        it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () => {
             //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            console.log(process.env.BASE_URL)
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -15,7 +17,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
@@ -34,7 +36,7 @@ describe('Transferências', () => {
        it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
         
         //Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -44,7 +46,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
